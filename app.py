02b78ae937df8364a9f515f46624cbab46dc10d1
Sarthakischauhan <sarthakchauhann@gmail.com>
2022-03-20 03:50:54 -0700

    consensus and nodes

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 from blockchain import Blockchain
 from flask import Flask,jsonify,request
 from uuid import uuid4
+import sys
+
+PORT = sys.argv[1]
 
 app = Flask(__name__)
 
@@ -13,7 +16,6 @@ node_identifier = str(uuid4()).replace('-', '')
 @app.route("/mine",methods=["GET"])
 def mine():
 	last_block = blockchain.last_block,
-	print(last_block[0])	
 	last_proof = last_block[0]["proof"]
 	
 
@@ -56,6 +58,50 @@ def get_chain():
 	}
 	return jsonify(response),200
 
+@app.route("/nodes/register",methods=["POST"])
+def register_nodes():
+	values = request.get_json()
+	nodes = values.get("nodes")
+
+	if not nodes:
+		return "Error: Please supply a valid list of nodes",400
+
+	for node in nodes:
+		blockchain.register_nodes(node)
+
+	response = {
+			"message":"New nodes have been added",
+			"total_nodes":list(blockchain.nodes)
+		}
+
+	return jsonify(response),201
+
+
+@app.route("/nodes/all",methods=["GET"])
+def get_nodes():
+	return {"nodes":list(blockchain.nodes)}
+
+@app.route("/nodes/resolve",methods=["GET"])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+				"message":"Our chain was changed",
+				"new_chain":blockchain.chain	
+		}
+
+	else :
+		response = {
+				"message":"Our chain is authoritative",
+				"chain":blockchain.chain
+		}
+
+	return jsonify(response),200
+
+
+
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(port=PORT,debug=True)
--- blockchain.py
+++ blockchain.py
@@ -2,14 +2,15 @@
 import json 
 import hashlib
 from time import time
-
+from urllib.parse import urlparse
+import requests
 
 class Blockchain:
 	
 	def __init__(self):
 		self.chain = []
 		self.current_transaction = []
-
+		self.nodes = set()
 		self.new_block(100,1)
 		
 	def new_block(self,proof,previous_hash=None):
@@ -53,3 +54,56 @@ class Blockchain:
 			proof += 1
 
 		return proof
+
+	def register_nodes(self,address):
+		parsed_url = urlparse(address)
+		self.nodes.add(parsed_url.netloc)
+	
+	# So a conflict is when two nodes have different blockchains
+	# The longest chain among the two node will be considered rest blocks will be orphaned
+	def valid_chain(self,chain):
+		previous_block = chain[0]
+		index = 1
+
+		while index < len(chain):
+			block = chain[index]
+
+			if block["previous_hash"] != self.hash(previous_block):
+				return False
+
+			if not self.valid_proof(block["proof"],previous_block["proof"]):
+				return False
+
+			previous_block = block
+			index += 1
+		
+		return True
+
+	
+	def resolve_conflicts(self):
+		neighbours = self.nodes
+		new_chain = None
+
+		max_len = len(self.chain)
+       
+		for node in neighbours:
+			response = requests.get(f'http://{node}/chain')
+			print(response.status_code)
+			if response.status_code == 200:
+				print("run")
+				length = response.json()['length']
+				chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+				if length > max_len and self.valid_chain(chain):
+					max_len = length
+					new_chain = chain
+		
+		if new_chain:
+			print(new_chain)
+			self.chain = new_chain
+			return True
+
+		return False
+
+
